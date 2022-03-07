595b071cbf4ee2457ddd351fbe08296f0b648236
Seth Thomas <sthomas@seths-mbp-2.lan>
2022-03-07 15:09:46 -0500

    Did some reformatting with rubocop

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 # Class that will hold all of the cells that players will change
 class Board
-    attr_reader :grid
+  attr_reader :grid
 
   def initialize
     @grid = create_blank_grid
@@ -9,43 +11,43 @@ class Board
   def create_blank_grid
     blank_grid = []
     cell_number = 0
-    until blank_grid.length === 9 do
+    until blank_grid.length == 9
       new_cell = Cell.new
       new_cell.change_grid_number(cell_number)
       blank_grid << new_cell
       cell_number += 1
     end
-    return blank_grid
+    blank_grid
   end
 
   def display_grid_numbers
-    puts ""
+    puts ''
     puts "#{@grid[0].grid_number} | #{@grid[1].grid_number} | #{@grid[2].grid_number}"
-    puts "--+---+--"
+    puts '--+---+--'
     puts "#{@grid[3].grid_number} | #{@grid[4].grid_number} | #{@grid[5].grid_number}"
-    puts "--+---+--"
+    puts '--+---+--'
     puts "#{@grid[6].grid_number} | #{@grid[7].grid_number} | #{@grid[8].grid_number}"
-    puts ""
+    puts ''
   end
 
   def display_symbols
-    puts ""
+    puts ''
     puts "#{@grid[0].symbol} | #{@grid[1].symbol} | #{@grid[2].symbol}"
-    puts "--+---+--"
+    puts '--+---+--'
     puts "#{@grid[3].symbol} | #{@grid[4].symbol} | #{@grid[5].symbol}"
-    puts "--+---+--"
+    puts '--+---+--'
     puts "#{@grid[6].symbol} | #{@grid[7].symbol} | #{@grid[8].symbol}"
-    puts ""
+    puts ''
   end
 
   def display_game_interface
-    puts ""
+    puts ''
     puts "#{@grid[0].game_display} | #{@grid[1].game_display} | #{@grid[2].game_display}"
-    puts "--+---+--"
+    puts '--+---+--'
     puts "#{@grid[3].game_display} | #{@grid[4].game_display} | #{@grid[5].game_display}"
-    puts "--+---+--"
+    puts '--+---+--'
     puts "#{@grid[6].game_display} | #{@grid[7].game_display} | #{@grid[8].game_display}"
-    puts ""
+    puts ''
   end
 
   def change_symbol_to_x_at(grid_index)
@@ -57,8 +59,6 @@ class Board
   end
 
   def change_user_input_to_index(user_num_input)
-    index = user_num_input - 1
-    return index
+    user_num_input - 1
   end
-
 end
