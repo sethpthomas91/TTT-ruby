bcea48805c5ef6d5599cdba4364494ecccbbf7a9
Seth Thomas <sthomas@seths-mbp-2.lan>
2022-03-09 13:54:44 -0500

    display moved from board class to ui

--- lib/board.rb
+++ lib/board.rb
@@ -22,36 +22,6 @@ class Board
     blank_grid
   end
 
-  def display_grid_numbers
-    puts ''
-    puts "#{@grid[0].grid_number} | #{@grid[1].grid_number} | #{@grid[2].grid_number}"
-    puts '--+---+--'
-    puts "#{@grid[3].grid_number} | #{@grid[4].grid_number} | #{@grid[5].grid_number}"
-    puts '--+---+--'
-    puts "#{@grid[6].grid_number} | #{@grid[7].grid_number} | #{@grid[8].grid_number}"
-    puts ''
-  end
-
-  def display_symbols
-    puts ''
-    puts "#{@grid[0].symbol} | #{@grid[1].symbol} | #{@grid[2].symbol}"
-    puts '--+---+--'
-    puts "#{@grid[3].symbol} | #{@grid[4].symbol} | #{@grid[5].symbol}"
-    puts '--+---+--'
-    puts "#{@grid[6].symbol} | #{@grid[7].symbol} | #{@grid[8].symbol}"
-    puts ''
-  end
-
-  def display_game_interface
-    puts ''
-    puts "#{@grid[0].game_display} | #{@grid[1].game_display} | #{@grid[2].game_display}"
-    puts '--+---+--'
-    puts "#{@grid[3].game_display} | #{@grid[4].game_display} | #{@grid[5].game_display}"
-    puts '--+---+--'
-    puts "#{@grid[6].game_display} | #{@grid[7].game_display} | #{@grid[8].game_display}"
-    puts ''
-  end
-
   def change_symbol_to_x_at(grid_index)
     @grid[grid_index].make_x
   end
