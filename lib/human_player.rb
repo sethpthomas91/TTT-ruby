6021cda25ade45b5cbdc12080dd4df4ff4262608
Seth Thomas <sthomas@seths-mbp-2.lan>
2022-03-14 18:36:27 -0400

    Human vs Computer working, need terminal prompt to switch modes

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class HumanPlayer
-  attr_reader :marker
+  attr_reader :marker, :is_computer
 
-  def initialize(marker = 'X')
+  def initialize(marker = 'X', is_computer = false)
     @marker = marker
+    @is_computer = is_computer
   end
 
   def assign_marker_as(new_marker)
