a857c8c1581b5cf25bcb2b58fc4d2f49ea360779
Scott Plunkett <c_splunkett@groupon.com>
2022-03-22 17:18:22 -0500

    [ADDED] Specs using objects w/ stubbed methods w/ comments.

--- spec/game_setup_spec.rb
+++ spec/game_setup_spec.rb
@@ -26,16 +26,70 @@ RSpec.describe GameSetup do
       expect(something).to eq('1')
     end
   end
-# FAILING TEST BELOW
+
   describe '#computer_difficulty_setter' do
-    it 'should recieve user input and set player to ubeatable' do
-      allow($stdin).to receive(:gets).and_return('1')
-      player = ComputerPlayer.new
-      allow(game_setup.computer_difficulty_setter(player)).to receive($stdin) { '1' }
-      expect(player.is_unbeatable).to eq(true)
+    # Mocking/stubbing can be difficult to wrap your mind around.
+    # Run the test and look over the below specs/notes.
+    # docs:
+    # https://relishapp.com/rspec/rspec-mocks/docs
+    context 'user inputs 1' do
+      it 'sets player to unbeatable' do
+        player = ComputerPlayer.new
+        allow_any_instance_of(UI).to receive(:prompt_set_unbeatable).with(player)
+        allow_any_instance_of(UI).to receive(:binary_choice_input) { 1 }
+        # `{ 1 }` at the end of the above line is an aliass for `and_return(1)`
+        game_setup.computer_difficulty_setter(player)
+        expect(player.is_unbeatable).to eq(true)
+      end
+      #   ABOVE ^^^^
+      #   since the SUT (system under test) is not interacting
+      #   directly with $stdin/$stdout you don't need to stub those,
+      #   instead we can provide method stubs
+      #   for the class that calls them, saying:
+      #   "Hey UI, instead of behaving normally when
+      #    AN INSTANCE OF YOUR CLASS receives the message
+      #   `:prompt_set_unbeatable` with `player`, just
+      #   ALLOW the message to be received but bypass the
+      #   method/behavior. And when AN INSTANCE OF YOUR CLASS receives
+      #   `:binary_choice_input` than return 1."
+
+      # vvvv BELOW
+      # Another way we can write this is using expect vs allow.
+      # This makes the receiving of the message not just allowed but expected,
+      # more like an assertion; meaning it will fail if the object with the
+      # method stub doesn't receive the message whereas the allow syntax
+      # would not cause a failure.
+      it 'sets player to unbeatable (using expect vs. allow)' do
+        player = ComputerPlayer.new
+        expect_any_instance_of(UI).to receive(:prompt_set_unbeatable).with(player)
+        expect_any_instance_of(UI).to receive(:binary_choice_input) { 1 }
+        game_setup.computer_difficulty_setter(player)
+        expect(player.is_unbeatable).to eq(true)
+      end
+    end
+
+    context 'user inputs 2' do
+      it 'sets player to beatable' do
+        player = ComputerPlayer.new
+        allow_any_instance_of(UI).to receive(:prompt_set_unbeatable).with(player)
+        allow_any_instance_of(UI).to receive(:binary_choice_input) { 2 }
+        game_setup.computer_difficulty_setter(player)
+        expect(player.is_unbeatable).to eq(false)
+      end
     end
   end
-# FAILING TEST ABOVE
+
+  # FAILING TEST BELOW
+  # describe '#computer_difficulty_setter' do
+  #   it 'should recieve user input and set player to ubeatable' do
+  #     allow($stdin).to receive(:gets).and_return('1')
+  #     player = ComputerPlayer.new
+  #     allow(game_setup.computer_difficulty_setter(player)).to receive($stdin) { '1' }
+  #     expect(player.is_unbeatable).to eq(true)
+  #   end
+  # end
+  # FAILING TEST ABOVE
+
   describe '#set_unbeatable_computer' do
     it 'should set the computer player as unbeatable' do
       game_setup = GameSetup.new
