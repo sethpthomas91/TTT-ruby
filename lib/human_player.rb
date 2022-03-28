87b3d40b655916071ae3863a2765feff6068c774
Seth Thomas <sthomas@seths-mbp-2.lan>
2022-03-28 11:24:06 -0400

    Seperated out a smaller interface for human game interface
    Ended dependency on other player when calling player.move

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -1,20 +1,23 @@
 require_relative './player'
+require_relative './human_game_interface'
 
 class HumanPlayer < Player
+  attr_reader :marker, :is_computer
   def initialize(marker = 'X', is_computer: false)
     @marker = marker
     @is_computer = is_computer
+    @human_game_interface = HumanGameInterface.new
   end
 
-  def move(board:, maximizing_player:)
+  def move(board:)
     user_input = get_integer_between(1, 9)
-    user_input = check_is_valid_human_move(user_input, board)
+    user_input = check_is_valid_human_move(user_input: user_input, board:board)
   end
 
-  def check_is_valid_human_move(user_input, board)
+  def check_is_valid_human_move(user_input:, board:)
     valid_move = board.valid_move?(user_input)
     while valid_move == false
-      prompt_invalid_space(user_input)
+      @human_game_interface.prompt_invalid_space(user_input)
       user_input = get_integer_between(1, 9)
       valid_move = board.valid_move?(user_input)
     end
@@ -31,7 +34,7 @@ class HumanPlayer < Player
       user_integer = input_to_int(user_input)
       break if is_valid_input_between(user_integer, lower_int, upper_int)
 
-      prompt_invalid_input(lower_int, upper_int)
+      @human_game_interface.prompt_invalid_input(lower_int, upper_int)
     end
     user_integer
   end
@@ -43,13 +46,4 @@ class HumanPlayer < Player
   def input_to_int(user_input)
     user_input.to_i
   end
-
-  def prompt_invalid_input(lower_int, upper_int)
-    puts "Please enter a valid selection between #{lower_int} and #{upper_int}"
-  end
-
-  def prompt_invalid_space(invalid_user_input)
-    puts "#{invalid_user_input} is already played. \nPlease enter a valid move"
-  end
-
 end
